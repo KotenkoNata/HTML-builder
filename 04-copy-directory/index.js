6bdac39932ab3af32e8f69a1d05952a5c618b41c
KotenkoNata <kotenkonata8@gmail.com>
2022-05-19 14:59:31 +0300

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
 
+fs.exists(`${__dirname}/files-copy`, (exists) => {
+  if(!exists){
+    fs.promises.mkdir(`${__dirname}/files-copy`).then(function() {
+      console.log('Directory created successfully');
+    }).catch(function() {
+      console.log('Failed to create directory');
+    });
+  }
+});
+
+fs.readdir(`${__dirname}/files`,
+  { withFileTypes: true },
+  (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      files.forEach(file => {
+        fs.promises.copyFile(`${__dirname}/files/${file.name}`, `${__dirname}/files-copy/${file.name}`)
+          .then(function () {
+            console.log('File copied');
+          }).catch(function (err) {
+            console.log(err);
+          });
+      });
+    }
+  });
