ce555ffd917f751613a27f681baf4e7e0d1e6bec
KotenkoNata <kotenkonata8@gmail.com>
2022-05-23 10:52:14 +0300

    refactor: solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
 
-fs.exists(`${__dirname}/files-copy`, (exists) => {
+const FromFolderPath = path.join(__dirname, 'files');
+const ToFolderPath = path.join(__dirname, 'files-copy');
+
+fs.exists(ToFolderPath, (exists) => {
   if(!exists){
-    fs.promises.mkdir(`${__dirname}/files-copy`).then(function() {
+    fsPromises.mkdir(`${__dirname}/files-copy`, {recursive: true}).then(function() {
       console.log('Directory created successfully');
     }).catch(function() {
       console.log('Failed to create directory');
@@ -10,19 +15,30 @@ fs.exists(`${__dirname}/files-copy`, (exists) => {
   }
 });
 
-fs.readdir(`${__dirname}/files`,
-  { withFileTypes: true },
-  (err, files) => {
-    if (err)
-      console.log(err);
-    else {
-      files.forEach(file => {
-        fs.promises.copyFile(`${__dirname}/files/${file.name}`, `${__dirname}/files-copy/${file.name}`)
-          .then(function () {
-            console.log('File copied');
-          }).catch(function (err) {
-            console.log(err);
-          });
-      });
-    }
-  });
+async function copiedFolder(FromFolderPath, ToFolderPath){
+  await fsPromises.rm(ToFolderPath, {force: true, recursive: true});
+  await fsPromises.mkdir(ToFolderPath, {recursive: true});
+
+  fs.readdir(FromFolderPath,
+    { withFileTypes: true },
+    (err, files) => {
+      if (err)
+        console.log(err);
+      else {
+        files.forEach(file => {
+          fsPromises.copyFile(path.join(FromFolderPath, `${file.name}`), path.join(ToFolderPath,`${file.name}`))
+            .then(function () {
+              console.log('File copied');
+            }).catch(function (err) {
+              console.log(err);
+            });
+        });
+      }
+    });
+}
+
+copiedFolder(FromFolderPath, ToFolderPath).then(r => console.log(r));
+
+
+
+
